7b8edc1f5bbf4025b8a4e196cf07522da3fe08ca
marioszocs <szocs.mario@gmail.com>
2022-12-13 19:39:22 +0100

    Initial commit

---
+++ src/main/java/dev/marioszocs/hotelreservationapi/HotelReservationApiApplication.java
@@ -0,0 +1,13 @@
+package dev.marioszocs.hotelreservationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelReservationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelReservationApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/marioszocs/hotelreservationapi/HotelReservationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.marioszocs.hotelreservationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelReservationApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
